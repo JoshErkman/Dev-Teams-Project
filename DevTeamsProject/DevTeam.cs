ee843dd3535bf134f93cc98ab5a8f02c823919d1
Josh Erkman <erkman.josh@gmail.com>
2020-11-24 15:18:26 -0500

    initial commit

---
+++ DevTeamsProject/DevTeam.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTeamsProject
+{
+    public enum ProjectStatus
+    {
+        PrePhase = 1,
+        Pending,
+        Complete,
+    }
+    public class DevTeam
+    {
+        public string Title { get; set; }
+
+        public string TeamID { get; set; }
+        public string Department { get; set; }
+
+        public string ProjectID { get; set; }
+
+        public ProjectStatus StatusOfProject { get; set; }
+
+        public List<Developer> Members { get; set; }
+
+        public DevTeam() { }
+
+        public DevTeam(string title, string teamID, string department, string projectID, ProjectStatus statusOfProject, List<Developer> members,)
+        {
+            Title = title;
+            TeamID = teamID;
+            Department = department;
+            ProjectID = projectID;
+            StatusOfProject = statusOfProject;
+            Members = members;
+        }
+    }
+}
---
+++ DevTeamsProject/DeveloperRepo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTeamsProject
+{
+    public class DeveloperRepo
+    {
+        private readonly List<Developer> _developerDirectory = new List<Developer>();
+
+        //Developer Create
+        public void AddDevToList(Developer developer)
+        {
+            _developerDirectory.Add(developer);
+        }
+
+        // Generte random ID number
+
+       /* public Random a = new Random();
+        public List<int> randomList = new List<int>();
+        int MyNumber = 0;
+        private void NewNumber()
+        {
+            MyNumber = a.Next(0, 10);
+            while (randomList.Contains(MyNumber))
+                MyNumber = a.Next(0, 10);
+        }*/
+
+
+
+        //Developer Read
+        public List<Developer> GetListOfDevs()
+        {
+            return _developerDirectory;
+        }
+
+
+        //Developer Update
+        public bool UpdateExistingDev(string originalDeveloper, Developer newDev)
+        {
+            // Find the original developer the user would like to update
+            Developer originalDev = GetDeveloperByID(originalDeveloper);
+
+            // Update the developer in the list
+            if(originalDev != null)
+            {
+                originalDev.Name = newDev.Name;
+                originalDev.IDNumber = newDev.IDNumber;
+                originalDev.HasPluralsightAccess = newDev.HasPluralsightAccess;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        //Developer Delete
+        public bool RemoveDevFromList(string developer)
+        {
+            Developer developerNeeded = GetDeveloperByID(developer);
+            if(developerNeeded == null)
+            {
+                return false;
+            }
+
+            int initialCount = _developerDirectory.Count;
+            _developerDirectory.Remove(developerNeeded);
+
+            if(initialCount > _developerDirectory.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+
+        //Developer Helper (Get Developer by ID)
+
+        public Developer GetDeveloperByID(string developerID)
+        {
+            foreach(Developer id in _developerDirectory)
+            {
+                if(id.IDNumber.ToLower() == developerID.ToLower())
+                {
+                    return id;
+                }
+            }
+
+            return null;
+        }
+
+
+    }
+}
